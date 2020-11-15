31926720041af2299305f8d67840fad549022357
Raj <manrajparmar793@gmail.com>
2020-11-15 20:18:45 +0530

    Added yml file.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Test, Edit <code>src/App.js</code> and save to reload.
+          Testing, Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
